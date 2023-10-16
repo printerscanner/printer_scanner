2cc2e5568ac53ca89b4d03c2377d3bc7abd3033d
printer_scanner <abbey@fictivekin.com>
2023-10-16 14:17:38 +0200

    lazy load

--- src/App.jsx
+++ src/App.jsx
@@ -73,8 +73,8 @@ function App() {
             key={id}
           >
             <>
-              {data[id].thumbnail && !data[id].mov && <img src={data[id].thumbnail} alt={data[id].title} />}
-              {data[id].mov && <video autoPlay={true} muted={true}><source autoPlay src={data[id].mov} type="video/mp4" /></video>}
+              {data[id].thumbnail && !data[id].mov && <img loading="lazy" src={data[id].thumbnail} alt={data[id].title} />}
+              {data[id].mov && <video loading="lazy" autoPlay={true} muted={true}><source autoPlay src={data[id].mov} type="video/mp4" /></video>}
               <div className="text">
                 <p>{data[id].link ? <span>↗ </span> : ''} <b>{data[id].title}</b></p>
                 <p style={{fontSize: '12px', paddingBottom: '.5rem'}}>{data[id].subcategory ? <span className="grid-crumbs">{data[id].subcategory}</span> : ''}
