d4adfd1cefa0ac360aefb2cad64f40afe8ada104
printer_scanner <abbey@fictivekin.com>
2024-05-15 17:58:35 +0200

    update

--- src/Post.jsx
+++ src/Post.jsx
@@ -1,16 +1,23 @@
 import { useParams } from 'react-router-dom'
 
 import data from './data/api.js'
+import Header from './Header.jsx';
 
 function Post() {
 
 	const { id } = useParams();
 	const product = data[id]
 	return (
+		<>
+		<Header/>
 		<article>
-			<div className="blog">
-				<a href="/" className="home-button"><u>←</u></a>
-					<h2>{product.title}</h2>
+			<a href="/" className="home-button"><u>←</u></a>
+
+
+				<div className="grid-layout large-grid">
+				<div className="blog grid-item span-2 text">
+					<h1 className='visually-hidden'>{product.title}</h1>
+					<b>{product.title}</b>
 					<h5>{product.subtitle}</h5>
 					<p>{product.description}</p>
 					<div className={product.content ? '' : 'hidden'} dangerouslySetInnerHTML={{__html: product.content }} />
@@ -18,7 +25,6 @@ function Post() {
 					<a className={product.githubLink ? '' : 'hidden'} href={product.githubLink}><u>↗ Fork it on Github</u></a>
 					<a className={product.listen ? '' : 'hidden'} href={product.listen}><u>↗ Listen</u></a>
 				</div>
-				<div className="grid-layout large-grid">
 					{product.img1 && <div className="grid-item span-2"><img className="product-subhero" src={product.img1} alt={product.title} loading="lazy"></img></div>}
 					{product.img2 && <div className="grid-item span-2"><img className="product-subhero" src={product.img2} alt={product.title} loading="lazy"></img></div>}
 					{product.img3 && <div className="grid-item span-2"><img className="product-subhero" src={product.img3} alt={product.title} loading="lazy"></img></div>}
@@ -28,6 +34,7 @@ function Post() {
 					{product.mov && <div className="grid-item span-2"><video autoPlay={true} loop={true} muted={true} width="400px" height="auto"><source autoPlay loop src={product.mov} type="video/mp4" /></video></div>}
 				</div>
 			</article>
+		</>
 		)
 	}
 
