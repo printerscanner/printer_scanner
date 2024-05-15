21c1f7e2ba95d914d5acc0f4c21322d970f189bb
printer_scanner <abbey@fictivekin.com>
2024-05-15 19:43:30 +0200

    update

--- src/Form.jsx
+++ src/Form.jsx
@@ -3,26 +3,17 @@ function Form() {
 
   return (
 
-<form name="contact" method="POST" data-netlify="true">
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
+    <form 
+    name="contact" 
+    method="POST" 
+    data-netlify="true" 
+   >
+  <input type="hidden" name="form-name" value="contact" />
+     <input id="standard-basic" label="name" name="name" />
+     <input id="standard-basic" label="email" name="email" />
+     <input id="standard-basic" label="message" name="message" />
+     <button type="submit">Send</button>
+    </form>
 
   );
 }
