101e0f2a12151343c38674bbb75cb8edda808588
Abbey Yacoe <abbey@xyflow.com>
2024-12-23 15:24:42 +0100

    Update api.js

--- src/data/api.js
+++ src/data/api.js
@@ -1,138 +1,156 @@
-
-
 const data = {
-	"pruner-js": {
-		title: "Pruner.js",
-		collaborators: "Headless Horse",
-		collaboratorsUrl: "https://headless.horse",
-		category: "Utilities",
-		software: "JavaScript",
-		role: "Development",
-		license: "MIT",
-		year: "2024",
-		source: "https://github.com/overbrowsing/pruner",
-		description: "pruner.js is a JavaScript utility that enhances image handling by stitching multiple images into a single canvas. ",
-		descriptionContinued: "It works by dividing large images into smaller tiles and only loading the necessary tiles based on the viewport size. This approach not only reduces the total amount of data transferred but also improves performance by ensuring that only visible parts of the image are processed. By minimising data transfer and leveraging efficient image management, pruner.js accelerates loading times and supports sustainable web design practices."
-	},
-	"stylelint-sustainable-css": {
-		title: "Stylelint Sustainable CSS",
-		collaborators: "Headless Horse",
-		collaboratorsUrl: "https://headless.horse",
-		category: "Utilities",
-		software: "Stylelint, CSS",
-		role: "Development",
-		license: "MIT",
-		year: "2024",
-		source: "https://github.com/overbrowsing/stylelint-sustainable-css#readme",
-		description: "Sustainable CSS is a Stylelint plugin dedicated to following sustainable web design principles."
-	},
-	"protean-magazine": {
-		title: "Protean Magazine",
-		category: "Website",
-		software: "Wordpress, WooCommerce",
-		role: "Design, Development",
-		featured: "TRUE",
-		year: "2024",
-		source: "https://proteanmag.com",
-		description: "Protean publishes incisive and affecting criticism, journalism, poetry, and art for the discerning leftist, sans advertisements."
-	},
-	"the-garden-of-earthly-delights": {
-		title: "The Garden of Earthly Delights",
-		category: "Website",
-		software: "Obsidian, Eleventy",
-		featured: "TRUE",
-		year: "2024",
-		source: "https://earthly-delights.net",
-		description: "A blog about life's perils and temptations."
-	},
-	"mother-type": {
-		title: "Mother Type",
-		category: "Type",
-		software: "React",
-		license: "SIL Open Font License (OFL)",
-		featured: "TRUE",
-		year: "2024",
-		source: "https://mother-type.de",
-		description: "Mother Type is an open-source type foundry exploring an open model for typefaces that are free to use, modify, and distribute under the SIL Open Font License (OFL)."
-	},
-	"overbrowsing": {
-		title: "Overbrowsing",
-		collaborators: "Headless Horse",
-		collaboratorsUrl: "https://headless.horse",
-		category: "Website",
-		software: "Nunjucks, Are.na",
-		featured: "TRUE",
-		year: "2024",
-		source: "https://overbrowsing.com",
-		description: "A super low-carbon website for an applied research group dedicated to advancing our understanding of sustainable web design and development practises.",
-	},
-	"coaster-sans": {
-		title: "Coaster Sans",
-		collaborators: "Mother Type",
-		collaboratorsUrl: "https://mother-type.de",
-		category: "Type",
-		software: "Glyphs",
-		year: "2023",
-		source: "https://mother-type.de/fonts/coaster-sans",
-		description: "A new chunky display font that will make you feel like you're riding the waves back home."
-	},
-	"my-body-sans": {
-		title: "(my) body sans",
-		collaborators: "Mother Type",
-		collaboratorsUrl: "https://mother-type.de",
-		category: "Type",
-		software: "Glyphs",
-		year: "2023",
-		source: "https://mother-type.de/fonts/body-sans",
-		description: "You've seen kitsch body fonts, but you haven't seen one that's literally my body."
-	},
-	"galaxy": {
-		title: "Galaxy",
-		collaborators: "Fictive Kin",
-		collaboratorsUrl: "https://fictivekin.com",
-		category: "Website",
-		software: "Contentful, 11ty",
-		year: "2023",
-		source: "https://galaxy.com/",
-		description: "A websystem for a cryptocurrency investment firm."
-	},
-	"everlaw": {
-		title: "Everlaw",
-		collaborators: "Fictive Kin",
-		collaboratorsUrl: "https://fictivekin.com",
-		category: "Website",
-		software: "Contentful, 11ty",
-		year: "2023",
-		source: "https://everlaw.com/",
-		description: "A websystem for a platform for document analysis for law firms, government, and corporations."
-	},
-	"pjt": {
-		title: "PJT",
-		collaborators: "Fictive Kin",
-		collaboratorsUrl: "https://fictivekin.com",
-		category: "Website",
-		software: "Contentful, 11ty",
-		year: "2023",
-		source: "https://pjtpartners.com/",
-		description: "A websystem for an advisory-focused investment bank."
-	},
-	"sapiens-hate-club": {
-		title: "Sapiens Hate Club",
-		category: "Website",
-		year: "2023",
-		source: "https://sapienshate.club",
-		description: "Please join us. Please."
-	},
+  "co2-shield": {
+    title: "CO2 Shield",
+    collaborators: "Headless Horse",
+    collaboratorsUrl: "https://headless.horse",
+    category: "Utilities",
+    software: "TypeScript",
+    role: "Development",
+    license: "MIT",
+    year: "2024",
+    source: "https://github.com/overbrowsing/pruner",
+    description:
+      "Calculate the environmental impact of a webpage and display it in your GitHub project as a shields.io badge. ",
+  },
+  "pruner-js": {
+    title: "Pruner.js",
+    collaborators: "Headless Horse",
+    collaboratorsUrl: "https://headless.horse",
+    category: "Utilities",
+    software: "JavaScript",
+    role: "Development",
+    license: "MIT",
+    year: "2024",
+    source: "https://github.com/overbrowsing/pruner",
+    description:
+      "pruner.js is a JavaScript utility that enhances image handling by stitching multiple images into a single canvas. ",
+    descriptionContinued:
+      "It works by dividing large images into smaller tiles and only loading the necessary tiles based on the viewport size. This approach not only reduces the total amount of data transferred but also improves performance by ensuring that only visible parts of the image are processed. By minimising data transfer and leveraging efficient image management, pruner.js accelerates loading times and supports sustainable web design practices.",
+  },
+  "stylelint-sustainable-css": {
+    title: "Sustainable CSS",
+    collaborators: "Headless Horse",
+    collaboratorsUrl: "https://headless.horse",
+    category: "Utilities",
+    software: "Stylelint, CSS",
+    role: "Development",
+    license: "MIT",
+    year: "2024",
+    source: "https://github.com/overbrowsing/stylelint-sustainable-css#readme",
+    description:
+      "Sustainable CSS is a Stylelint plugin dedicated to following sustainable web design principles.",
+  },
+  "protean-magazine": {
+    title: "Protean Magazine",
+    category: "Website",
+    software: "Wordpress, WooCommerce",
+    role: "Design, Development",
+    featured: "TRUE",
+    year: "2024",
+    source: "https://proteanmag.com",
+    description:
+      "Protean publishes incisive and affecting criticism, journalism, poetry, and art for the discerning leftist, sans advertisements.",
+  },
+  "the-garden-of-earthly-delights": {
+    title: "The Garden of Earthly Delights",
+    category: "Website",
+    software: "Obsidian, Eleventy",
+    featured: "TRUE",
+    year: "2024",
+    source: "https://earthly-delights.net",
+    description: "A blog about life's perils and temptations.",
+  },
+  "mother-type": {
+    title: "Mother Type",
+    category: "Type",
+    software: "React",
+    license: "SIL Open Font License (OFL)",
+    featured: "TRUE",
+    year: "2024",
+    source: "https://mother-type.de",
+    description:
+      "Mother Type is an open-source type foundry exploring an open model for typefaces that are free to use, modify, and distribute under the SIL Open Font License (OFL).",
+  },
+  overbrowsing: {
+    title: "Overbrowsing",
+    collaborators: "Headless Horse",
+    collaboratorsUrl: "https://headless.horse",
+    category: "Website",
+    featured: "TRUE",
+    year: "2024",
+    source: "https://overbrowsing.com",
+    description:
+      "A super low-carbon website for an applied research group dedicated to advancing our understanding of sustainable web design and development practises.",
+  },
+  "coaster-sans": {
+    title: "Coaster Sans",
+    collaborators: "Mother Type",
+    collaboratorsUrl: "https://mother-type.de",
+    category: "Type",
+    software: "Glyphs",
+    year: "2023",
+    source: "https://mother-type.de/fonts/coaster-sans",
+    description:
+      "A new chunky display font that will make you feel like you're riding the waves back home.",
+  },
+  "my-body-sans": {
+    title: "(my) body sans",
+    collaborators: "Mother Type",
+    collaboratorsUrl: "https://mother-type.de",
+    category: "Type",
+    software: "Glyphs",
+    year: "2023",
+    source: "https://mother-type.de/fonts/body-sans",
+    description:
+      "You've seen kitsch body fonts, but you haven't seen one that's literally my body.",
+  },
+  galaxy: {
+    title: "Galaxy",
+    collaborators: "Fictive Kin",
+    collaboratorsUrl: "https://fictivekin.com",
+    category: "Website",
+    software: "Contentful, 11ty",
+    year: "2023",
+    source: "https://galaxy.com/",
+    description: "A websystem for a cryptocurrency investment firm.",
+  },
+  everlaw: {
+    title: "Everlaw",
+    collaborators: "Fictive Kin",
+    collaboratorsUrl: "https://fictivekin.com",
+    category: "Website",
+    software: "Contentful, 11ty",
+    year: "2023",
+    source: "https://everlaw.com/",
+    description:
+      "A websystem for a platform for document analysis for law firms, government, and corporations.",
+  },
+  pjt: {
+    title: "PJT",
+    collaborators: "Fictive Kin",
+    collaboratorsUrl: "https://fictivekin.com",
+    category: "Website",
+    software: "Contentful, 11ty",
+    year: "2023",
+    source: "https://pjtpartners.com/",
+    description: "A websystem for an advisory-focused investment bank.",
+  },
+  "sapiens-hate-club": {
+    title: "Sapiens Hate Club",
+    category: "Website",
+    year: "2023",
+    source: "https://sapienshate.club",
+    description: "Please join us. Please.",
+  },
 
-	"slightly-better-css": {
-		title: "Slightly Better CSS",
-		category: "Utilities",
-		featured: "TRUE",
-		year: "2022",
-		source: "https://github.com/printerscanner/slightly-better-css",
-		description: "A webpage without any CSS is slightly too ugly, libraries like Bootstrap are way too heavy."
-	}
- };
- 
- export default data;
- 
+  "slightly-better-css": {
+    title: "Slightly Better CSS",
+    category: "Utilities",
+    year: "2022",
+    source: "https://github.com/printerscanner/slightly-better-css",
+    description:
+      "A webpage without any CSS is slightly too ugly, libraries like Bootstrap are way too heavy.",
+  },
+};
+
+export default data;
